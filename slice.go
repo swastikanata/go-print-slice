cac42fc10defd52eb50a1e1ece32aebbdf573fef
swastikanata <swastika.nata1804@gmail.com>
2025-02-20 10:54:36 +0700

    export modules

--- slice.go
+++ slice.go
@@ -2,13 +2,13 @@ package slice
 
 import "fmt"
 
-func print1DSlice(slice []int) {
+func Print1DSlice(slice []int) {
 	for i := 0; i < len(slice); i++ {
 		fmt.Println(slice[i])
 	}
 }
 
-func print2DSlice(slice [][]int) {
+func Print2DSlice(slice [][]int) {
 	for i := 0; i < len(slice); i++ {
 		for j := 0; j < len(slice[i]); j++ {
 			fmt.Println(slice[i][j])
