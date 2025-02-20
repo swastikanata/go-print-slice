ed6591dbb8c987a97ceb85d0393a77e49b49b019
swastikanata <swastika.nata1804@gmail.com>
2025-02-20 11:10:49 +0700

    new commit

--- slice.go
+++ slice.go
@@ -1,17 +1,27 @@
 package slice
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func Print1DSlice(slice []int) {
+	cellWidth := 4
 	for i := 0; i < len(slice); i++ {
-		fmt.Println(slice[i])
+		fmt.Printf("%*d", cellWidth, slice[i])
 	}
 }
 
 func Print2DSlice(slice [][]int) {
+	cellWidth := 4
 	for i := 0; i < len(slice); i++ {
 		for j := 0; j < len(slice[i]); j++ {
-			fmt.Println(slice[i][j])
+			if slice[i][j] == 0 {
+				fmt.Print(strings.Repeat(" ", cellWidth))
+			} else {
+				fmt.Printf("%*d", cellWidth, slice[i][j])
+			}
 		}
+		fmt.Println()
 	}
 }
